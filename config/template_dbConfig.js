67b37446157882a1d69652d0d681a56e94f925d3
U-LKAB\mlbjos <mlbjos@LKLI05714.corp.lkab.com>
2021-04-06 22:12:00 +0200

    change in template config

--- config/template_dbConfig.js
+++ config/template_dbConfig.js
@@ -1,10 +1,14 @@
+
 "use strict";
 
+// This is a template for the Database configuration
+// enter correct values and change the name of the file to dbConfig.js
+
 const dbConfig = {
-    user: 'test',
-    password: 'Password1!',
+    user: 'username',
+    password: 'password',
     server: "localhost", // You can use 'localhost\\instance' to connect to named instance
-    database: 'LKTelereg',
+    database: 'Database',
     options: {
         encrypt: true,
         enableArithAbort: false
